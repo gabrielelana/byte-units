eb92ed81ec4b8c17cde85a64bbad77d853112fba
gabriele.lana <gabriele.lana@cleancode.it>
2014-06-20 12:02:27 +0200

    Extract base and suffixes as private static attributes

--- src/ByteUnits/Binary.php
+++ src/ByteUnits/Binary.php
@@ -4,11 +4,12 @@ namespace ByteUnits;
 
 class Binary extends System
 {
+    private static $base = 1024;
+    private static $suffixes = ['YiB'=>8, 'ZiB'=>7, 'EiB'=>6, 'PiB'=>5, 'TiB'=>4, 'GiB'=>3, 'MiB'=>2, 'KiB'=>1, 'B'=>0];
+
     public function __construct($numberOfBytes, $precision = self::NORMAL_PRECISION)
     {
-        $base = 1024;
-        $suffixes = ['YiB'=>8, 'ZiB'=>7, 'EiB'=>6, 'PiB'=>5, 'TiB'=>4, 'GiB'=>3, 'MiB'=>2, 'KiB'=>1, 'B'=>0];
-        $scale = new PowerScale($base, $suffixes, self::MAXIMUM_PRECISION);
+        $scale = new PowerScale(self::$base, self::$suffixes, self::MAXIMUM_PRECISION);
         $formatter = new Formatter($scale, $precision);
         parent::__construct($numberOfBytes, $formatter);
     }
--- src/ByteUnits/Metric.php
+++ src/ByteUnits/Metric.php
@@ -4,11 +4,12 @@ namespace ByteUnits;
 
 class Metric extends System
 {
+    private static $base = 1000;
+    private static $suffixes = ['YB'=>8, 'ZB'=>7, 'EB'=>6, 'PB'=>5, 'TB'=>4, 'GB'=>3, 'MB'=>2, 'kB'=>1, 'B'=>0];
+
     public function __construct($numberOfBytes, $precision = self::NORMAL_PRECISION)
     {
-        $base = 1000;
-        $suffixes = ['YB'=>8, 'ZB'=>7, 'EB'=>6, 'PB'=>5, 'TB'=>4, 'GB'=>3, 'MB'=>2, 'kB'=>1, 'B'=>0];
-        $scale = new PowerScale($base, $suffixes, self::MAXIMUM_PRECISION);
+        $scale = new PowerScale(self::$base, self::$suffixes, self::MAXIMUM_PRECISION);
         $formatter = new Formatter($scale, $precision);
         parent::__construct($numberOfBytes, $formatter);
     }
