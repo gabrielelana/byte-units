718802b4d9f8684abee79e9498ed32a0db11aca9
gabriele.lana <gabriele.lana@cleancode.it>
2014-06-22 23:08:59 +0200

    Extract scale as static attribute in Metric

--- src/ByteUnits/Metric.php
+++ src/ByteUnits/Metric.php
@@ -6,17 +6,16 @@ class Metric extends System
 {
     private static $base = 1000;
     private static $suffixes = ['YB'=>8, 'ZB'=>7, 'EB'=>6, 'PB'=>5, 'TB'=>4, 'GB'=>3, 'MB'=>2, 'kB'=>1, 'B'=>0];
+    private static $scale;
 
     public function __construct($numberOfBytes, $precision = self::NORMAL_PRECISION)
     {
-        $scale = new PowerScale(self::$base, self::$suffixes, self::MAXIMUM_PRECISION);
-        $formatter = new Formatter($scale, $precision);
-        parent::__construct($numberOfBytes, $formatter);
+        self::$scale = new PowerScale(self::$base, self::$suffixes, self::MAXIMUM_PRECISION);
+        parent::__construct($numberOfBytes, new Formatter(self::$scale, $precision));
     }
 
     public static function parser()
     {
-        $scale = new PowerScale(self::$base, self::$suffixes, self::MAXIMUM_PRECISION);
-        return new Parser($scale, __CLASS__);
+        return new Parser(self::$scale, __CLASS__);
     }
 }
