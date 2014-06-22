9465609e430979c328dfc35e9cacd9d5b0e11338
gabriele.lana <gabriele.lana@cleancode.it>
2014-06-22 23:10:14 +0200

    Extract scale as static attribute in Binary

--- src/ByteUnits/Binary.php
+++ src/ByteUnits/Binary.php
@@ -6,17 +6,16 @@ class Binary extends System
 {
     private static $base = 1024;
     private static $suffixes = ['YiB'=>8, 'ZiB'=>7, 'EiB'=>6, 'PiB'=>5, 'TiB'=>4, 'GiB'=>3, 'MiB'=>2, 'KiB'=>1, 'B'=>0];
+    private static $scale;
 
     public function __construct($numberOfBytes, $precision = self::NORMAL_PRECISION)
     {
-        $scale = new PowerScale(self::$base, self::$suffixes, self::MAXIMUM_PRECISION);
-        $formatter = new Formatter($scale, $precision);
-        parent::__construct($numberOfBytes, $formatter);
+        self::$scale = new PowerScale(self::$base, self::$suffixes, self::MAXIMUM_PRECISION);
+        parent::__construct($numberOfBytes, new Formatter(self::$scale, $precision));
     }
 
     public static function parser()
     {
-        $scale = new PowerScale(self::$base, self::$suffixes, self::MAXIMUM_PRECISION);
-        return new Parser($scale, __CLASS__);
+        return new Parser(self::$scale, __CLASS__);
     }
 }
