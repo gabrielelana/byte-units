06784c57c2f106ade7072a0bf74e3e9370ccab23
gabriele.lana <gabriele.lana@cleancode.it>
2014-06-22 23:12:05 +0200

    Extract parser as static attribute in Metric/Binary

--- src/ByteUnits/Binary.php
+++ src/ByteUnits/Binary.php
@@ -7,6 +7,7 @@ class Binary extends System
     private static $base = 1024;
     private static $suffixes = ['YiB'=>8, 'ZiB'=>7, 'EiB'=>6, 'PiB'=>5, 'TiB'=>4, 'GiB'=>3, 'MiB'=>2, 'KiB'=>1, 'B'=>0];
     private static $scale;
+    private static $parser;
 
     public function __construct($numberOfBytes, $precision = self::NORMAL_PRECISION)
     {
@@ -16,6 +17,6 @@ class Binary extends System
 
     public static function parser()
     {
-        return new Parser(self::$scale, __CLASS__);
+        return self::$parser = self::$parser ?: new Parser(self::$scale, __CLASS__);
     }
 }
--- src/ByteUnits/Metric.php
+++ src/ByteUnits/Metric.php
@@ -7,6 +7,7 @@ class Metric extends System
     private static $base = 1000;
     private static $suffixes = ['YB'=>8, 'ZB'=>7, 'EB'=>6, 'PB'=>5, 'TB'=>4, 'GB'=>3, 'MB'=>2, 'kB'=>1, 'B'=>0];
     private static $scale;
+    private static $parser;
 
     public function __construct($numberOfBytes, $precision = self::NORMAL_PRECISION)
     {
@@ -16,6 +17,6 @@ class Metric extends System
 
     public static function parser()
     {
-        return new Parser(self::$scale, __CLASS__);
+        return self::$parser = self::$parser ?: new Parser(self::$scale, __CLASS__);
     }
 }
